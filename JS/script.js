a251ea78fd0e9b6febe9bd52a1e2a247beec7f58
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-02-05 13:46:10 +0000

    quick output

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,22 @@
-let input = document.getElementsByTagName('h1')[0].textContent;
-console.log(input);
 
-$.getJSON(`https://api.teleport.org/api/urban_areas/slug:${input}/scores/`, function(data){//success callback function
+let inputTimeout;
+$("input").on("input", function(){
+    clearTimeout(inputTimeout);
+    inputTimeout = setTimeout(function() {
+        let input = document.getElementById('input').value;
+        console.log(input);
+        console.log(input);
+        $.getJSON(`https://api.teleport.org/api/urban_areas/slug:${input}/scores/`, function(data){//success callback function
         let items = [];// will store items of the loop below
         $.each(data.categories, function(key, val) {
             items.push("<li>" + val.name + ": " + val.score_out_of_10 + "</li>");
         });
-        //creating ul, adding items inside and appending to body
+        //creating ul, adding string of items to the innerHTML of ul and appending to body
         $("<ul/>", {
             "class": "list",
             html: items.join("")
         }).appendTo("body");
+    });
+    }, 500)
 });
+
