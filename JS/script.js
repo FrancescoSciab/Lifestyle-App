0934c22839da3727bdaa85c04d39f92360095c5d
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-02-06 13:12:52 +0000

    search bar working. To add error handler

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,34 @@
-
+//It's a global variable to keep track of the setTimeout call that is created when the input event fires
 let inputTimeout;
+let createdUl;
 $("input").on("input", function(){
+    // to ensure that a new timeout will be set each time the input event is triggered
     clearTimeout(inputTimeout);
+    //To execute the function 0.5 seconds later user has stopped typing
     inputTimeout = setTimeout(function() {
         let input = document.getElementById('input').value;
-        console.log(input);
-        console.log(input);
+        if (!input) {
+            if (createdUl) {
+                console.log("ciao");
+                createdUl.remove();
+            }
+            return;
+        }
+        if(!input || $(`ul.city-${input}`).length){
+            return;
+        }
         $.getJSON(`https://api.teleport.org/api/urban_areas/slug:${input}/scores/`, function(data){//success callback function
         let items = [];// will store items of the loop below
         $.each(data.categories, function(key, val) {
             items.push("<li>" + val.name + ": " + val.score_out_of_10 + "</li>");
         });
         //creating ul, adding string of items to the innerHTML of ul and appending to body
-        $("<ul/>", {
-            "class": "list",
-            html: items.join("")
+        createdUl = $("<ul/>", {
+            "class": `list city-${input}`,
+            html: `<h2> ${input} </h2>` + items.join("")
         }).appendTo("body");
     });
-    }, 500)
+    }, 500);
+    
 });
 
