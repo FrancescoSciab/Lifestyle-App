7dc51b91f0e26c3afa36e032a8a5deb7ad06062d
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-02-09 16:04:57 +0000

    js checked

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,46 +11,51 @@ $("input").on("input", function(){
     inputTimeout = setTimeout(function() {
         //1: getting input value
         let input = document.getElementById('input').value;
+        //2: making it match the correct slug
+        let formattedInput = input.toLowerCase().split(" ").join("-");
 
 
-        if(!input || $(`ul.city-${input}`).length){//if ul exists, its lenght will be greater than 0
+        if(!input.toLowerCase || $(`ul.city-${formattedInput}`).length){//if ul exists, its lenght will be greater than 0
             return;
         }
 
-        if (!input) {//user deleted or pressed x
+        if (!formattedInput) {//user deleted or clicked x
             if (createdUl) {
                 createdUl.remove();//to remove what user requested
             }
             return;
         }
 
-        //2: API call
-        $.getJSON(`https://api.teleport.org/api/urban_areas/slug:${input}/scores/`, function(data){//success callback function
+        //3: API call
+        $.getJSON(`https://api.teleport.org/api/urban_areas/slug:${formattedInput}/scores/`, function(data){//success callback function
 
-        //3: will store items of the loop below
+        //action 1: will store items of the loop below
         let items = []; 
         
-        //4: loop and push
+        //action 2: loop and push
         $.each(data.categories, function(key, val) {
             items.push("<li><strong>" + val.name + "</strong>" + ": " + val.score_out_of_10 + "</li>");
         });
 
 
-        //5: creating ul, adding string of items to the innerHTML of ul and appending to selected div
+        //action 3: creating ul, adding string of items to the innerHTML of ul and appending to selected div
         createdUl = $("<ul/>", {
-            "class": `list city-${input}`,
+            "class": `list city-${formattedInput}`,
             html: `<h2> ${input} </h2>` + items.join("")
         }).appendTo("#categories");
 
-        //6: error handler function
+        //error handler function
         }).fail(function(){
-            let error = $("<div/>", {
-                "class": "error-text position-relative text-danger",
+            //fail action 1:
+            let error = $("<p/>", {
+                "class": "text-danger",
                 "html": "City not available"
             }).appendTo('#label');
+            //fail action 2:
             setTimeout(function(){
                 error.remove()
             }, 2000);
+            //fail action 3:
             console.error("city not available!");
         });
     }, 500);
