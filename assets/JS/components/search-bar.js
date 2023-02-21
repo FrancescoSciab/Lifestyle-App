af61c4d44668dd755b52a4ef710a814dbd3e90e7
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-02-21 12:18:35 +0000

    version 1.2

--- assets/JS/components/search-bar.js
+++ assets/JS/components/search-bar.js
@@ -1,11 +1,11 @@
+import 'jquery';
 import _ from 'lodash';
 //It's a global variable to keep track of the setTimeout call that is created when the input event fires
 let inputTimeout;
 let categoriesUl;
 
 
-export const searchBar = $(document).ready( function(){
-    $("input").on("input", function(){
+export const searchBar = $("input").on("input", function(){
     
         // to ensure that a new timeout will be set each time the input event is triggered
         clearTimeout(inputTimeout);
@@ -13,8 +13,8 @@ export const searchBar = $(document).ready( function(){
         //runs 0.5s later user finishes typing
         inputTimeout = setTimeout(function() {
            
-            let cityInput = document.getElementById('input').value;
-            let formattedCityInput = cityInput.toLowerCase().split(" ").join("-");
+            const cityInput = document.getElementById('input').value;
+            const formattedCityInput = cityInput.toLowerCase().split(" ").join("-");
             
             //conditions which prevent API call
             if($(`ul.city-${formattedCityInput}`).length){//if ul exists, its lenght will be greater than 0
@@ -34,7 +34,7 @@ export const searchBar = $(document).ready( function(){
                 $.getJSON(`https://api.teleport.org/api/urban_areas/slug:${formattedCityInput}/scores/`, function(data){
 
                     //will store items of the loop below
-                    let items = [];
+                    const items = [];
                     const categories = _.get(data, 'categories', 0);
                     $.each(categories, function(key, val) {
                         items.push("<li><strong>" + val.name + "</strong>" + ": " + val.score_out_of_10 + "</li>");
@@ -45,14 +45,14 @@ export const searchBar = $(document).ready( function(){
                         "class": `list city-${formattedCityInput}`,
                         html: `<h2> ${cityInput} </h2>` + `<li> ${summaries} </li>` + items.join("")
                     }).appendTo("#categories");//selected div (*)
-                }).fail (function(){
-                    const error = $("<p/>", {
+                }).fail (function(){ //errors that occur during the AJAX request
+                    const fail = $("<p/>", {
                         "class": "text-danger",//boostrap info
                         "html": "City not available"
                     }).appendTo('#label');
         
                     setTimeout(function(){
-                        error.remove()
+                        fail.remove()
                     }, 2000);
                     
                     console.error("city not available or city name misspelled");
@@ -65,4 +65,3 @@ export const searchBar = $(document).ready( function(){
             }
         }, 500);
     })
-});
